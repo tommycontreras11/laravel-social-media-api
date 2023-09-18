8327b437a2fb258b4f1a731d28189963a1dbe588
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-09-18 09:30:14 -0400

    Added user controller to manage request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,21 +46,124 @@ class UserController extends Controller
         ];
     }
 
-    public function store(Request $request)
-    {
-        //
-    }
-
+    /**
+     * Show user information
+     * @OA\Get (
+     *     path="/api/users/{id}",
+     *     tags={"User"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="number")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="username", type="string", example="Tommy 11"),
+     *              @OA\Property(property="first_name", type="string", example="Tommy"),
+     *              @OA\Property(property="last_name", type="string", example="Grullón Contreras"),
+     *              @OA\Property(property="telephone", type="string", example="829-754-6150"),
+     *              @OA\Property(property="age", type="int", example="20"),
+     *              @OA\Property(property="created_at", type="string", example="2023-02-23T00:09:16.000000Z"),
+     *              @OA\Property(property="updated_at", type="string", example="2023-02-23T12:33:45.000000Z")
+     *         )
+     *     )
+     * )
+     */
     public function show(string $id)
     {
         //
     }
 
+    /**
+     * Update user information
+     * @OA\Patch (
+     *     path="/api/users/{id}",
+     *     tags={"User"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="number")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="username",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="first_name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="last_name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="telephone",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="age",
+     *                          type="number"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "username":"Tommy 11",
+     *                     "first_name":"Tommy",
+     *                     "last_name":"Grullón Contreras",
+     *                     "telephone":"829-754-6150",
+     *                     "age":20
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="CREATED",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="username", type="string", example="Tommy 11"),
+     *              @OA\Property(property="first_name", type="string", example="Tommy"),
+     *              @OA\Property(property="last_name", type="string", example="Grullón Contreras"),
+     *              @OA\Property(property="telephone", type="string", example="829-754-6150"),
+     *              @OA\Property(property="age", type="number", example=20),
+     *              @OA\Property(property="created_at", type="string", example="2023-02-23T00:09:16.000000Z"),
+     *              @OA\Property(property="updated_at", type="string", example="2023-02-23T12:33:45.000000Z")
+     *          )
+     *      )
+     * )
+     */
     public function update(Request $request, string $id)
     {
         //
     }
 
+    /**
+     * Delete user information
+     * @OA\Delete (
+     *     path="/api/users/{id}",
+     *     tags={"User"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="number")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="NO CONTENT"
+     *     )
+     * )
+     */
     public function destroy(string $id)
     {
         //
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('username', 50)->unique();
+            $table->string('telephone');
+            $table->integer('age');
+            $table->string('email', 50)->unique();
+            $table->string('password', 32)->min(6);
             $table->timestamps();
         });
     }
