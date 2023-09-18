c13b943539c570768f4cb33785bcb59047f15662
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-09-18 15:47:47 -0400

    Added method to get the user profile logged in and sign out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Http\Responses\ApiResponse;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -82,8 +83,176 @@ class AuthController extends Controller
         ]);
     }
 
-    public function create(Request $request)
+    /**
+     * Create a new user
+     * @OA\Post (
+     *     path="/api/auth/register",
+     *     tags={"Auth"},
+     *     description="Sign in",
+     *     operationId="register",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="username",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="first_name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="last_name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="telephone",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="age",
+     *                          type="number"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      ),
+     *                 ),
+     *                 example={
+     *                     "first_name":"Tommy",
+     *                     "last_name":"Grullón Contreras",
+     *                     "username":"Tommy11",
+     *                     "email":"tommy@gmail.com",
+     *                     "telephone":"829-754-6150",
+     *                     "age":20,
+     *                     "password":"Hola1234"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="CREATED",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="first_name", type="string", example="Tommy"),
+     *              @OA\Property(property="last_name", type="string", example="Grullón Contreras"),
+     *              @OA\Property(property="username", type="string", example="Tommy11"),
+     *              @OA\Property(property="email", type="string", example="tommy@gmail.com"),
+     *              @OA\Property(property="telephone", type="string", example="829-754-6150"),
+     *              @OA\Property(property="age", type="number", example=20),
+     *              @OA\Property(property="password", type="string", example="Hola1234"),
+     *              @OA\Property(property="created_at", type="string", example="2023-02-23T00:09:16.000000Z"),
+     *              @OA\Property(property="updated_at", type="string", example="2023-02-23T12:33:45.000000Z")
+     *          )
+     *      )
+     * )
+     */
+    public function register(Request $request)
     {
-        
+        try {
+            $request->validate([
+                'first_name' => ['required', 'string'],
+                'last_name' => ['required', 'string'],
+                'username' => ['required', 'string', 'unique:users,username'],  
+                'telephone' => ['string'],                
+                'age' => ['required', 'integer'],              
+                'email' => ['required', 'email', 'unique:users,email'],
+                'password' => ['required', 'string', 'min:6']
+            ]);
+
+            $user = User::create(array_merge(
+                $request->all(),
+                ['password' => Hash::make($request->password, [
+                    'rounds' => 6
+                ])]
+            ));
+
+            return ApiResponse::success('The user has been successfully created', 200, new UserResource($user));
+        } catch (Exception $e) {
+            return ApiResponse::error('Error: ' . $e->getMessage(), 422);
+        } 
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/auth/profile",
+     *     tags={"Auth"},
+     *     summary="Profile",
+     *     operationId="Profile",
+     *     security={{"token": {}}},
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *   @OA\Response(
+     *      response=403,
+     *      description="Forbidden"
+     *   )
+     *)
+     **/
+    public function profile() 
+    {
+        return ApiResponse::success('Success', 200, new UserResource(auth()->user()));
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     tags={"Auth"},
+     *     summary="Logout",
+     *     operationId="Logout",
+     *     security={{"token": {}}},
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *   @OA\Response(
+     *      response=403,
+     *      description="Forbidden"
+     *   )
+     *)
+     **/
+    public function logout(Request $request) 
+    {
+        $request->user()->token()->revoke();
+        return ApiResponse::success('Successfully logged out', 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api', 'verified'], function() {    
+    //Auth
+    Route::get('/auth/profile', [AuthController::class, 'profile']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
     //User
     Route::get('/users', [UserController::class, 'index']);
 });
