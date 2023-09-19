1693e1a9df796aafd912cf83e9c8d3619e00a58c
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-09-19 16:25:12 -0400

    Added post comment controller to manage request

---
+++ database/factories/PostCommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PostComment>
+ */
+class PostCommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'post_id' => DB::table('posts')->inRandomOrder()->value('id'),
+            'user_id' => DB::table('users')->inRandomOrder()->value('id'),
+            'comment' => fake()->paragraph(2, false)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Post;
+use App\Models\PostComment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //User::factory(10)->create();
-        Post::factory(10)->create();
+        //Post::factory(10)->create();
+        PostComment::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
