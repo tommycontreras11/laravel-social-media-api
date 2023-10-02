81672eda8452838fc585ba33df7e42f02f31ac09
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-02 10:18:07 -0400

    Added migration and relationship between user and user_friend

---
+++ app/Models/UserFriend.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserFriend extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'source_id', 'target_id', 'type', 'status',
+    ];
+
+    public function source_friend(): BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+}
